c12563fe81a02d5e68166312c668ca3e4ffca8b1
Guilherme Luvizute <luvizute@gmail.com>
2024-05-06 17:30:42 -0300

    Criação e e configuração do banco de dados SQLite

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 var (
 	db     *gorm.DB
@@ -8,7 +12,18 @@ var (
 )
 
 func InitDB() error {
+	var err error
+
+	// Initialize database
+	db, err = InitSQLite()
+	if err != nil {
+		return fmt.Errorf("Erro ao inicializar o banco de dados: %v", err)
+	}
 	return nil
+}
+
+func GetDB() *gorm.DB {
+	return db
 }
 
 func InitLogger(prefix string) *Logger {
---
+++ config/sqlite.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"os"
+
+	"github.com/guilherme-luvi/go-api-gin-swagger-goorm-sqlite/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitSQLite() (*gorm.DB, error) {
+	logger := InitLogger("sqlite")
+	dbPath := "./db/main.db"
+
+	// check if database file exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("Arquivo do Banco de dados sqlite não encontrado, criando um novo...")
+
+		// create db directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+
+		// create db file
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+		logger.Info("Aquivo do banco de dados sqlite criado com sucesso")
+	}
+
+	// connect to sqlite database
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("Erro ao conectar com o banco de dados sqlite: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("Erro ao criar a tabela Opening: %v", err)
+		return nil, err
+	}
+
+	logger.Info("Conexão com o banco de dados sqlite estabelecida com sucesso")
+	return db, nil
+}
